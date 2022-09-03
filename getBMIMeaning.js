05a90ca3825672b0d15686d86f133cd159674553
Supatsorn Poonsawat <supatsorn.supa@mail.kmutt.ac.th>
2022-09-04 01:50:15 +0700

    HW02

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -6,6 +6,15 @@ function calculateBMI(weight, height) {
  
 }
 function getBMIMeaning(weight, height) {
-  
+  calculateBMI(weight, height)
+  if (BMI < 18.5){
+  return "Underweight"
+} else if(BMI <= 24.9){
+  if(BMI >= 18.5){
+    return "Normal weight"
+  }
+}else if (BMI > 25.0 ){
+  return "Overweight"
+}
 }
 module.exports = getBMIMeaning
